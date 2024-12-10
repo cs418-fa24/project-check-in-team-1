413cb390301089865c2821658279dccae58b49b4
Yash Patel <91900408+ypat0207@users.noreply.github.com>
2024-12-10 17:49:20 -0600

    Update model#1Ensemble.py

--- model#1Ensemble.py
+++ model#1Ensemble.py
@@ -8,8 +8,6 @@ from sklearn.pipeline import make_pipeline
 from sklearn.compose import ColumnTransformer
 
 file_path = 'ObesityDataSet.csv'
-
-
 categorical_cols = data.select_dtypes(include=['object']).columns.tolist()
 categorical_cols.remove('NObeyesdad') 
 numerical_cols = data.select_dtypes(include=['float64', 'int64']).columns.tolist()
@@ -19,13 +17,9 @@ preprocessor = ColumnTransformer(
         ('num', 'passthrough', numerical_cols),
         ('cat', OneHotEncoder(), categorical_cols)
     ])
-
-
 rf = RandomForestClassifier(random_state=42)
 svc = SVC(probability=True, random_state=42)
 gb = GradientBoostingClassifier(random_state=42)
-
-
 ensemble_model = VotingClassifier(
     estimators=[
         ('rf', rf),
@@ -34,18 +28,13 @@ ensemble_model = VotingClassifier(
     ],
     voting='soft' 
 )
-
-
 ensemble_pipeline = make_pipeline(preprocessor, ensemble_model)
 
 
 X = data.drop('NObeyesdad', axis=1)
 y = data['NObeyesdad']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-
 ensemble_pipeline.fit(X_train, y_train)
-
 y_pred = ensemble_pipeline.predict(X_test)
 report = classification_report(y_test, y_pred)
 print(report)
